9ff2a26fb22e3393d846ae3dab8d5fd493a48808
oleeeedev <oleschumacherr1801@gmail.com>
2025-02-02 23:39:25 +0100

    Add initial Vue 3 app setup with router and plugins
    This commit sets up the core structure for a Vue 3 application. It includes the integration of Pinia for state management, Vue Router for routing, Tailwind CSS for styling, and the click-outside-vue3 plugin. Basic configuration and imports are established to kickstart development.

---
+++ src/router.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+
+  }
+];
+
+//* create the router
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+
+
+export default router;
