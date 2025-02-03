63db106cf73dabf855c03c40793bff8884a8e581
oleeeedev <oleschumacherr1801@gmail.com>
2025-02-03 08:17:32 +0100

    Add UI components for empty, loading, and layout states
    Introduce reusable design elements for empty states (NoNotifications, NoMedia), loading states (Spinner, Circle2Lines, MultipleLines), and a basic structure for the HomeView layout. These components improve UI consistency and support better user feedback across various states. Add new utility and typography styles to enhance styling flexibility.

---
+++ src/components/views/HomeView/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <KeepAlive>
+    <div
+      class="xs:relative md:static h-full flex xs:flex-col md:flex-row overflow-hidden"
+      >
+
+    </div>
+  </KeepAlive>
+</template>
